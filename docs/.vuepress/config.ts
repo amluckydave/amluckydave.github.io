3c4ea6006210f0dcfd273af3da712cadde7b67b6
amluckydave <zhenjinxu@stu.xmu.edu.cn>
2025-10-28 09:20:17 +0800

    configTS

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -11,12 +11,12 @@ export default defineUserConfig({
   locales: {
     "/en/": {
       lang: "en-US",
-      title: "UIGF-org",
-      description: "Uniformed Interchangeable GenshinData Format",
+      title: "amluckydave-pro",
+      description: "the knowledge bank of amluckydave",
     },
     "/zh/": {
       lang: "zh-CN",
-      title: "UIGF-org",
+      title: "amluckydave-pro",
       description: "统一可交换原神数据标准",
     },
   },
